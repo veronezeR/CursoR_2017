8a94c372ce44c9d9bcfe6430f76b332cfae20e0b
veronezeR <33199955+veronezeR@users.noreply.github.com>
2017-11-06 12:22:44 -0200

    Update aula1.R

--- aula1.R
+++ aula1.R
@@ -1,6 +1,7 @@
 ###Primeiros passos no R
 
 #Descobrindo uma função - Quando você não sabe exatamente o que precisa
+
 help.search("termo da busca") #Procura uma função que seja similar ao termo buscado
 ??termo da busca #Equivalente a função help.search - um shortcut
 
@@ -9,3 +10,12 @@ help.search("termo da busca") #Procura uma função que seja similar ao termo bu
 
 help.search(“logarithms”)
 ??logarithm
+
+#Obtendo ajuda para uma função específica - Quando você já sabe exatamente qual função do R utilizar 
+
+help("nome da função")
+?nome da função #Equivalente ao help - um shortcut
+
+#Exemplo - buscando ajuda para a função mean  
+help(mean) #Retorna um R help file com descrição da função, argumentos que podem ser utilizados na função, exemplos, etc
+??mean
