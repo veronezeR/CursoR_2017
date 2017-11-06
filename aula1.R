aa587bc848d1f20f465bdf257cf366326f5b4bbf
veronezeR <33199955+veronezeR@users.noreply.github.com>
2017-11-06 12:14:49 -0200

    Update aula1.R

---
+++ aula1.R
@@ -0,0 +1,11 @@
+###Primeiros passos no R
+
+#Descobrindo uma função - Quando você não sabe exatamente o que precisa
+help.search("termo da busca") #Procura uma função que seja similar ao termo buscado
+??termo da busca #Equivalente a função help.search - um shortcut
+
+#Exemplo - buscando ajuda para calcular logarítimo 
+#termo da busca deve ser inserido em inglês
+
+help.search(“logarithms”)
+??logarithm
