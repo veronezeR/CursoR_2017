dc2fd1a93cdb2879bc7cd93a87981a3471b60b57
veronezeR <33199955+veronezeR@users.noreply.github.com>
2017-11-06 13:57:50 -0200

    Update aula1.R

--- aula1.R
+++ aula1.R
@@ -19,3 +19,61 @@ help("nome da função")
 #Exemplo - buscando ajuda para a função mean  
 help(mean) #Retorna um R help file com descrição da função, argumentos que podem ser utilizados na função, exemplos, etc
 ??mean
+
+#Qual é o meu diretório de trabalho??
+
+getwd()
+
+#Como mudar meu diretório de trabalho
+
+setwd("meu diretório") # Importante para direcionarmos o R para a pasta em em que se encontram os arquivos que iremos utilizar ou para pasta em que desejamos salvar aquivos e resultados
+
+###########################Exercício#################################
+
+#1. Crie um diretório para seus exercícios do curso.
+#2.Verifique o seu diretório de trabalho.
+#3.Mude o diretório de trabalho para o diretório que você criou.
+
+#######################################################################
+
+#O R funciona com pacotes
+#Ao instalar o R no seu computador automaticamente é instalado o pacote "base" o qual contém funções básicas
+#Para realizar tarefas mais complicadas ou específicas da sua área pode ser necessário instalar pacotes adicionais 
+#O pacotes são conjuntos de funções específicas do R, distribuídos em conjunto. 
+
+##Quais pacotes estão disponíveis no meu R?
+
+library()
+
+##Como instalar um pacote?
+
+install.packages("nome_do_pacote")
+
+##Exemplo - Vamos instalar o pacote ggplot2 que iremos utilizar futuramente no curso
+
+install.packages("ggplot2") # Pay attention - O R é case sensitive
+
+##Antes de utilizar funções de um pacote é preciso carregar o pacote
+
+library(nome_do_pacote) #função para carrega um pacote
+
+##Exemplo - Vamos carregar o pacote ggplot2
+
+library(ggplot2)
+
+#Como citar um pacote?
+
+citation("nome do pacote")
+
+citation("ggplot2")
+
+
+
+
+  
+
+
+
+
+
+
